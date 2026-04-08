f68da3a1ed8b064545b74fc5c388943ce750e4da
Andrew Bower <andrew.bower@amd.com>
2026-04-08 16:05:00 +0100

    ON-16268: update Debian packaging to use dh-python and pybuild

--- scripts/onload_misc/setup.py
+++ scripts/onload_misc/setup.py
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
 # X-SPDX-Copyright-Text: (c) Copyright 2011-2019 Xilinx, Inc.
-from distutils.core import setup
+#
+# Legacy definitions used for EL packaging, to be replaced
+# by pymodule.toml at the top level at some future point.
+#
+from setuptools import setup
 setup(name='OpenOnload utilities',
       version='1.0',
       author='David Riddoch',
